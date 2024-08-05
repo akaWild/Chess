1e021c8fc56d5940cb0f6a902ff8a98c92fb25e5
akaWild <nemyx84@gmail.com>
2024-08-05 20:30:59 +0300

    Added register user feature

---
+++ api/AuthService/Features/Register/RegisterEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using AuthService.DTOs;
+using Carter;
+using MediatR;
+
+namespace AuthService.Features.Register
+{
+    public class RegisterEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/auth/register", async (RegisterDto request, ISender sender) =>
+            {
+                var result = await sender.Send(new RegisterCommand(request));
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
