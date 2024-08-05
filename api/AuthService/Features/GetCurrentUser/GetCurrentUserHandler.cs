0b5a24a981e03005a4e480a56b139731310d35e4
akaWild <nemyx84@gmail.com>
2024-08-06 00:29:27 +0300

    Added get current user feature

---
+++ api/AuthService/Features/GetCurrentUser/GetCurrentUserHandler.cs
@@ -0,0 +1,46 @@
+﻿using AuthService.DTOs;
+using AuthService.Models;
+using AuthService.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SharedLib.CQRS;
+using SharedLib.Exceptions;
+using System.Security.Claims;
+
+namespace AuthService.Features.GetCurrentUser
+{
+    public record GetCurrentUserQuery : IQuery<UserDto>;
+
+    public class GetCurrentUserQueryHandler : IQueryHandler<GetCurrentUserQuery, UserDto>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly UserService _userService;
+        private readonly IHttpContextAccessor _context;
+
+        public GetCurrentUserQueryHandler(UserManager<AppUser> userManager, UserService userService, IHttpContextAccessor context)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _context = context;
+        }
+
+        public async Task<UserDto> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            var context = _context.HttpContext;
+            if (context == null)
+                throw new CustomErrorResponseException("Unable to get current user info", 400);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == context.User.FindFirstValue(ClaimTypes.Name), cancellationToken: cancellationToken);
+
+            if (user == null)
+                throw new CustomErrorResponseException("Unable to get current user info", 400);
+
+            var token = await context.GetTokenAsync("access_token");
+            if (token == null)
+                throw new CustomErrorResponseException("Unable to get current user info", 400);
+
+            return _userService.CreateUserDto(user, token);
+        }
+    }
+}
