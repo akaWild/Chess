822670b0f1338790fa6f45ebc29ac0baece88906
akaWild <nemyx84@gmail.com>
2024-08-05 20:31:49 +0300

    Configured app services

--- api/AuthService/Program.cs
+++ api/AuthService/Program.cs
@@ -1,5 +1,37 @@
+using AuthService.Data;
+using AuthService.Extensions;
+using AuthService.Features.Register;
+using Carter;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using SharedLib.Behaviors;
+using SharedLib.Exceptions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(RegisterCommand));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+DbInitializer.InitDb(app);
 
 app.Run();
