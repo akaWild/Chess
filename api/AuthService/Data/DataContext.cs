776a267844c19f3e737d5d3f3ad52b01c1e5ab91
akaWild <nemyx84@gmail.com>
2024-08-05 19:06:21 +0300

    Added data context

---
+++ api/AuthService/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
