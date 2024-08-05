64fbe31fb1f9424106fff4a930dea16a5b0c2361
akaWild <nemyx84@gmail.com>
2024-08-05 17:56:57 +0300

    Added custom exception handler

---
+++ api/SharedLib/Exceptions/CustomExceptionHandler.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharedLib.Exceptions
+{
+    public class CustomExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            (string Detail, string Title, int StatusCode) details = exception switch
+            {
+                ValidationException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    httpContext.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                CustomErrorResponseException responseException =>
+                (
+                    responseException.Message,
+                    responseException.GetType().Name,
+                    httpContext.Response.StatusCode = responseException.StatusCode
+                ),
+                _ =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
+                )
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = details.Title,
+                Detail = details.Detail,
+                Status = details.StatusCode,
+                Instance = httpContext.Request.Path
+            };
+
+            problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
+
+            if (exception is ValidationException validationException)
+                problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+
+            return true;
+        }
+    }
+}
