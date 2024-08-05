84f566f8b03181b82ecf64150904f697eb2a8cb4
akaWild <nemyx84@gmail.com>
2024-08-05 18:54:40 +0300

    Added user service

---
+++ api/AuthService/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.DTOs;
+using AuthService.Models;
+
+namespace AuthService.Services
+{
+    public class UserService
+    {
+        public UserDto CreateUserDto(AppUser user, string token)
+        {
+            return new UserDto(user.DisplayName, user.UserName, user.Email, token);
+        }
+    }
+}
