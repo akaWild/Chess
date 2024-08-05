adf41ac919d59b9dbc438b49a55cdd672a78e955
akaWild <nemyx84@gmail.com>
2024-08-05 10:22:34 +0300

    Added CQRS interfaces

---
+++ api/SharedLib/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SharedLib.CQRS
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
---
+++ api/SharedLib/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SharedLib.CQRS
+{
+    public interface IQueryHandler<in TQuery, TResponse>
+        : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
