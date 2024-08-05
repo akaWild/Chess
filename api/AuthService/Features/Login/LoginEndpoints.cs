43854b88c9dfc5fbe173d2968bbd7e26441cdfd5
akaWild <nemyx84@gmail.com>
2024-08-05 22:53:42 +0300

    Added user login feature

---
+++ api/AuthService/Features/Login/LoginEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using AuthService.DTOs;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Features.Login
+{
+    public class LoginEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/auth/login", async ([FromBody] LoginDto request, ISender sender) =>
+            {
+                var result = await sender.Send(new LoginCommand(request));
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
