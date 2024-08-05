1641c96c3fc43952aae5e2111f93ab27ac1b516b
akaWild <nemyx84@gmail.com>
2024-08-05 17:56:39 +0300

    Added custom response exception

---
+++ api/SharedLib/Exceptions/CustomErrorResponseException.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedLib.Exceptions
+{
+    public class CustomErrorResponseException : Exception
+    {
+        public int StatusCode { get; set; }
+        public CustomErrorResponseException(string message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
