0b450ed30695453d5f6c861f568e10333659419a
akaWild <nemyx84@gmail.com>
2024-08-05 20:31:49 +0300

    Configured app services

---
+++ api/AuthService/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using AuthService.Data;
+using AuthService.Models;
+using AuthService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthService.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireUppercase = true;
+                opt.Password.RequireDigit = true;
+                opt.Password.RequiredLength = 8;
+                opt.User.RequireUniqueEmail = true;
+
+            })
+            .AddEntityFrameworkStores<DataContext>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]!));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key
+                    };
+                });
+
+            services.AddAuthorization();
+            services.AddScoped<TokenService>();
+            services.AddScoped<UserService>();
+
+            return services;
+        }
+    }
+}
