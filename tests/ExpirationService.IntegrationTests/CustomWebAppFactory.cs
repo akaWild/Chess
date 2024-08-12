01d202fe6819a9958e1b1a718260f737ed6a15da
akaWild <nemyx84@gmail.com>
2024-08-13 01:36:55 +0300

    Added integration test for SideToActChanged event consumer

---
+++ tests/ExpirationService.IntegrationTests/CustomWebAppFactory.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace ExpirationService.IntegrationTests
+{
+    public class CustomWebAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.UseEnvironment("Testing");
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddMassTransitTestHarness();
+            });
+        }
+    }
+}
