0a1eb335df38800ad17caf6e5eda533e4cf18da4
akaWild <nemyx84@gmail.com>
2024-08-12 20:53:58 +0300

    Configured services

--- api/ExpirationService/Program.cs
+++ api/ExpirationService/Program.cs
@@ -1,8 +1,26 @@
+using ExpirationService.Consumers;
+using ExpirationService.Interfaces;
 using ExpirationService.Services;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddHostedService<CheckExpirationService>();
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<SideToActChangedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("exp", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest")!);
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest")!);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+builder.Services.AddHostedService<CheckMatchExpiredService>();
+builder.Services.AddSingleton<IExpirationCollection, ExpirationCollection>();
 
 var host = builder.Build();
 
