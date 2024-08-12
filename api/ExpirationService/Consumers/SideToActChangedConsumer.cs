1b3d78fbac23a2ecd5d997c0fbfc60d96983354b
akaWild <nemyx84@gmail.com>
2024-08-12 20:53:33 +0300

    Added SideToActChangedConsumer

---
+++ api/ExpirationService/Consumers/SideToActChangedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using EventsLib;
+using ExpirationService.Interfaces;
+using ExpirationService.Models;
+using MassTransit;
+
+namespace ExpirationService.Consumers
+{
+    public class SideToActChangedConsumer : IConsumer<SideToActChanged>
+    {
+        private readonly IExpirationCollection _expirationService;
+
+        public SideToActChangedConsumer(IExpirationCollection expirationService)
+        {
+            _expirationService = expirationService;
+        }
+
+        public Task Consume(ConsumeContext<SideToActChanged> context)
+        {
+            _expirationService.Remove(context.Message.MatchId);
+            _expirationService.Add(new MatchExpirationInfo(context.Message.MatchId, context.Message.SideToAct, context.Message.ExpTimeUtc));
+
+            return Task.CompletedTask;
+        }
+    }
+}
