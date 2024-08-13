f091e0b1c0173438a8b5dfa1d80f139c5a610b9d
akaWild <nemyx84@gmail.com>
2024-08-13 22:44:07 +0300

    Added match repository

---
+++ api/MatchService/Data/MatchRepository.cs
@@ -0,0 +1,35 @@
+﻿using MatchService.Interfaces;
+using MatchService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchService.Data;
+
+class MatchRepository : IMatchRepository
+{
+    private readonly DataContext _dbContext;
+
+    public MatchRepository(DataContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Match?> GetMatchById(Guid matchId)
+    {
+        return await _dbContext.Matches.FirstOrDefaultAsync(m => m.MatchId == matchId);
+    }
+
+    public void AddMatch(Match match)
+    {
+        _dbContext.Matches.Add(match);
+    }
+
+    public void RemoveMatch(Match match)
+    {
+        _dbContext.Remove(match);
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ api/MatchService/Interfaces/IMatchRepository.cs
@@ -0,0 +1,11 @@
+﻿using MatchService.Models;
+
+namespace MatchService.Interfaces;
+
+public interface IMatchRepository
+{
+    Task<Match?> GetMatchById(Guid matchId);
+    void AddMatch(Match match);
+    void RemoveMatch(Match match);
+    Task<int> SaveChangesAsync();
+}
