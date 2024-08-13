3676857bcb5ab54f5a9767a3706403a3d6a032af
akaWild <nemyx84@gmail.com>
2024-08-13 21:04:46 +0300

    Configured Db connection

--- api/MatchService/Program.cs
+++ api/MatchService/Program.cs
@@ -1,4 +1,13 @@
+using MatchService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
