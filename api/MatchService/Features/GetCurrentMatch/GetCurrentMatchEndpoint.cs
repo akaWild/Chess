120cb71e13fe922ac71c0933dea468ea1aae57d0
akaWild <nemyx84@gmail.com>
2024-08-14 20:39:23 +0300

    Added getting match info functionality (via SignalR)

---
+++ api/MatchService/Features/GetCurrentMatch/GetCurrentMatchEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MatchService.Features.GetCurrentMatch
+{
+    public partial class MatchHub : Hub
+    {
+        private readonly ISender _sender;
+
+        public MatchHub(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+
+            var httpContext = Context.GetHttpContext();
+            if (httpContext == null)
+                return;
+
+            var queryParam = httpContext.Request.Query["matchId"];
+
+            if (queryParam.Count == 0)
+                throw new HubException("Match Id wasn't provided");
+
+            var matchId = queryParam[0]!;
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, matchId);
+
+            if (!Guid.TryParse(matchId, out var matchIdGuid))
+                throw new HubException("Match Id has incorrect format");
+
+            try
+            {
+                var result = await _sender.Send(new GetCurrentMatchQuery(matchIdGuid));
+
+                await Clients.Caller.SendAsync("LoadMatchInfo", result);
+            }
+            catch (Exception e)
+            {
+                throw new HubException(e.Message);
+            }
+        }
+    }
+}
--- api/MatchService/Program.cs
+++ api/MatchService/Program.cs
@@ -1,16 +1,21 @@
 using MatchService.Data;
+using MatchService.Features.GetCurrentMatch;
+using MatchService.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddSignalR();
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHub<MatchHub>("matches");
 
 DbInitializer.InitDb(app);
 
