64fd892d609a486f9effaddfe218062ef3b4c6e2
akaWild <nemyx84@gmail.com>
2024-08-14 20:39:23 +0300

    Added getting match info functionality (via SignalR)

---
+++ api/MatchService/DTOs/MatchInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace MatchService.DTOs;
+
+public struct MatchInfo
+{
+    public required Guid MatchId { get; init; }
+    public required DateTime CreatedAtUtc { get; init; }
+    public DateTime? StartedAtUtc { get; init; }
+    public DateTime? EndedAtUtc { get; init; }
+    public DateTime? LastMoveMadeAtUtc { get; init; }
+    public int? TimeLimit { get; init; }
+    public int? ExtraTimePerMove { get; init; }
+    public string? WhiteSidePlayer { get; init; }
+    public required string Status { get; init; }
+    public required string Creator { get; init; }
+    public string? Acceptor { get; init; }
+    public int? AILevel { get; init; }
+    public string? Fen { get; init; }
+    public string? Pgn { get; init; }
+    public string? Winner { get; init; }
+    public string? WinBy { get; init; }
+    public string? DrawBy { get; init; }
+    public int? DrawRequestedSide { get; init; }
+    public int? WhiteSideTimeRemaining { get; init; }
+    public int? BlackSideTimeRemaining { get; init; }
+}
---
+++ api/MatchService/Features/GetCurrentMatch/GetCurrentMatchHandler.cs
@@ -0,0 +1,33 @@
+﻿using MatchService.DTOs;
+using MatchService.Interfaces;
+using SharedLib.CQRS;
+
+namespace MatchService.Features.GetCurrentMatch
+{
+    public record GetCurrentMatchQuery(Guid MatchId) : IQuery<MatchInfo>;
+
+    public class GetCurrentMatchHandler : IQueryHandler<GetCurrentMatchQuery, MatchInfo>
+    {
+        private readonly IMatchRepository _matchRepo;
+
+        public GetCurrentMatchHandler(IMatchRepository matchRepo)
+        {
+            _matchRepo = matchRepo;
+        }
+
+        public async Task<MatchInfo> Handle(GetCurrentMatchQuery request, CancellationToken cancellationToken)
+        {
+            var match = await _matchRepo.GetMatchById(request.MatchId);
+            if (match == null)
+                throw new Exception($"Match with id {request.MatchId} wasn't found");
+
+            return new MatchInfo
+            {
+                MatchId = match.MatchId,
+                CreatedAtUtc = match.CreatedAtUtc,
+                Creator = match.Creator,
+                Status = match.Status.ToString()
+            };
+        }
+    }
+}
