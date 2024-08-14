ce417906c827b5030b6f26e61e4e8b629d42a41f
akaWild <nemyx84@gmail.com>
2024-08-14 21:48:41 +0300

    Added automapper support

---
+++ api/MatchService/Utils/HistoryToPgnConverter.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ChessDotNet;
+
+namespace MatchService.Utils
+{
+    public class HistoryToPgnConverter : IValueConverter<string[], string>
+    {
+        public string Convert(string[] sourceMember, ResolutionContext context)
+        {
+            var chess = new Chess();
+
+            foreach (var move in sourceMember)
+                chess.Move(move);
+
+            return chess.GetPgn();
+        }
+    }
+}
---
+++ api/MatchService/Utils/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MatchService.DTOs;
+using MatchService.Models;
+
+namespace MatchService.Utils
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Match, MatchInfo>()
+                .ForMember(d => d.Fen, o => o.MapFrom(s => s.Board))
+                .ForMember(d => d.Pgn, o => o.ConvertUsing(new HistoryToPgnConverter(), s => s.History));
+        }
+    }
+}
