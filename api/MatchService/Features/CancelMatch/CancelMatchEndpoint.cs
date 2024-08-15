9eb86949c794812d979aa4012ee743228c00cdd8
akaWild <nemyx84@gmail.com>
2024-08-15 21:00:01 +0300

    Moved check for authenticated user from endpoint to handler

--- api/MatchService/Features/CancelMatch/CancelMatchEndpoint.cs
+++ api/MatchService/Features/CancelMatch/CancelMatchEndpoint.cs
@@ -1,5 +1,4 @@
-﻿using MatchService.Exceptions;
-using MatchService.Features.CancelMatch;
+﻿using MatchService.Features.CancelMatch;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -10,11 +9,7 @@ namespace MatchService.Features
         [Authorize]
         public async Task CancelMatch(Guid matchId)
         {
-            var username = Context.User?.Identity?.Name;
-            if (username == null)
-                throw new UserNotAuthenticated("User is not authenticated");
-
-            await _sender.Send(new CancelMatchCommand(matchId, username));
+            await _sender.Send(new CancelMatchCommand(matchId, Context.User?.Identity?.Name));
 
             await Clients.Group(matchId.ToString()).SendAsync("MatchCancelled", matchId);
         }
