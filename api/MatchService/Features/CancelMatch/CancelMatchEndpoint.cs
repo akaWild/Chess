0a29515050d3f08a7aaeb77de9adea47467e5b52
akaWild <nemyx84@gmail.com>
2024-08-15 20:27:21 +0300

    Added cancel match functionality

---
+++ api/MatchService/Features/CancelMatch/CancelMatchEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using MatchService.Exceptions;
+using MatchService.Features.CancelMatch;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MatchService.Features
+{
+    public partial class MatchHub
+    {
+        [Authorize]
+        public async Task CancelMatch(Guid matchId)
+        {
+            var username = Context.User?.Identity?.Name;
+            if (username == null)
+                throw new UserNotAuthenticated("User is not authenticated");
+
+            await _sender.Send(new CancelMatchCommand(matchId, username));
+
+            await Clients.Group(matchId.ToString()).SendAsync("MatchCancelled", matchId);
+        }
+    }
+}
