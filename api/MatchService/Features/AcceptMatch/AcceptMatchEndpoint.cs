2e515bd2a96c507bd94e646ffd80d2063d6735a5
akaWild <nemyx84@gmail.com>
2024-08-15 21:39:43 +0300

    Added match accepting functionality

---
+++ api/MatchService/Features/AcceptMatch/AcceptMatchEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using MatchService.Features.AcceptMatch;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MatchService.Features
+{
+    public partial class MatchHub
+    {
+        [Authorize]
+        public async Task AcceptMatch(Guid matchId)
+        {
+            await _sender.Send(new AcceptMatchCommand(matchId, Context.User?.Identity?.Name));
+
+            await Clients.Group(matchId.ToString()).SendAsync("MatchStarted", matchId);
+        }
+    }
+}
