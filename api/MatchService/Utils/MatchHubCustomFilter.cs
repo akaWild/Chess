0c5522a7f5efb170b8ed06a1dad0be4a24fbb307
akaWild <nemyx84@gmail.com>
2024-08-15 20:26:54 +0300

    Refactored MatchHubCustomFilter
    - Fixed condition for custom exception
    -Swapped ValueTask.CompletedTask to throw expression

--- api/MatchService/Utils/MatchHubCustomFilter.cs
+++ api/MatchService/Utils/MatchHubCustomFilter.cs
@@ -12,17 +12,17 @@ namespace MatchService.Utils
             {
                 return await next(invocationContext);
             }
-            catch (Exception e) when (e.GetType() == typeof(BaseClientException) || e.GetType() == typeof(ValidationException))
+            catch (Exception e) when (e is BaseClientException or ValidationException)
             {
                 await invocationContext.Hub.Clients.Caller.SendAsync("ClientError", e.Message);
 
-                return ValueTask.CompletedTask;
+                throw;
             }
             catch (Exception e)
             {
                 await invocationContext.Hub.Clients.Caller.SendAsync("ServerError", "Something went wrong");
 
-                return ValueTask.CompletedTask;
+                throw;
             }
         }
     }
