807a49cb82120cdc6b94f57b81948585ef81708d
akaWild <nemyx84@gmail.com>
2024-08-17 16:53:59 +0300

    Refactored GetCurrentMatchEndpoint

--- api/MatchService/Features/GetCurrentMatch/GetCurrentMatchEndpoint.cs
+++ api/MatchService/Features/GetCurrentMatch/GetCurrentMatchEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using MatchService.Features.GetCurrentMatch;
+﻿using MatchService.Exceptions;
+using MatchService.Features.GetCurrentMatch;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
 
@@ -23,19 +24,19 @@ namespace MatchService.Features
 
             var queryParam = httpContext.Request.Query["matchId"];
 
-            if (queryParam.Count == 0)
-                return;
+            if (queryParam.Count != 0)
+            {
+                var matchId = queryParam[0]!;
 
-            var matchId = queryParam[0]!;
+                await Groups.AddToGroupAsync(Context.ConnectionId, matchId);
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, matchId);
+                if (!Guid.TryParse(matchId, out var matchIdGuid))
+                    throw new MatchIdFormatException("Match Id has incorrect format");
 
-            if (!Guid.TryParse(matchId, out var matchIdGuid))
-                throw new HubException("Match Id has incorrect format");
+                var result = await _sender.Send(new GetCurrentMatchQuery(matchIdGuid));
 
-            var result = await _sender.Send(new GetCurrentMatchQuery(matchIdGuid));
-
-            await Clients.Caller.SendAsync("LoadMatchInfo", result);
+                await Clients.Caller.SendAsync("LoadMatchInfo", result);
+            }
         }
     }
 }
