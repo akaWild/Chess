9cef7d9e8a0e0a30b652fe6965758694227eb4f9
akaWild <nemyx84@gmail.com>
2024-08-21 19:52:44 +0300

    Added mapping profiles

---
+++ api/MatchService/Utils/FirstToActSideConverter.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace MatchService.Utils
+{
+    public class FirstToActSideConverter : IValueConverter<string?, int?>
+    {
+        public int? Convert(string? sourceMember, ResolutionContext context)
+        {
+            if (sourceMember == null)
+                return null;
+
+            if (int.TryParse(sourceMember, out var wsp))
+                return wsp;
+
+            return null;
+        }
+    }
+}
--- api/MatchService/Utils/MappingProfiles.cs
+++ api/MatchService/Utils/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using EventsLib;
 using MatchService.DTOs;
 using MatchService.Models;
 
@@ -13,6 +14,12 @@ namespace MatchService.Utils
                 .ForMember(d => d.Pgn, o => o.ConvertUsing(new HistoryToPgnConverter(), s => s.History));
 
             CreateMap<Match, MatchStartedDto>();
+
+            CreateMap<Match, MatchCreated>()
+                .ForMember(d => d.VsBot, o => o.MapFrom(s => s.AILevel != null))
+                .ForMember(d => d.FirstToActSide, o => o.ConvertUsing(new FirstToActSideConverter(), s => s.WhiteSidePlayer));
+
+            CreateMap<Match, MatchStarted>();
         }
     }
 }
