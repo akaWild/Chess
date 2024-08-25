617118135e486712e6b4d3ed90d72b6ca35c1cb5
akaWild <nemyx84@gmail.com>
2024-08-25 19:38:03 +0300

    Added tests for TimedOut event consumer

---
+++ tests/MatchService.IntegrationTests/TimedOutConsumerTests.cs
@@ -0,0 +1,148 @@
+﻿using AutoFixture;
+using EventsLib;
+using MassTransit.Internals;
+using MatchService.DTOs;
+using MatchService.IntegrationTests.Fixtures;
+using MatchService.IntegrationTests.Utils;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace MatchService.IntegrationTests
+{
+    [Collection("Shared collection")]
+    public class TimedOutConsumerTests : EndpointTestsBase
+    {
+        private readonly string? _tolianToken;
+        private readonly string? _kolianToken;
+
+        private MatchFinishedDto? _matchFinishedDto;
+
+        public TimedOutConsumerTests(CustomWebAppFactory factory) : base(factory)
+        {
+            _tolianToken = TokenHelper.GetAccessToken(factory, "Tolian");
+            _kolianToken = TokenHelper.GetAccessToken(factory, "Kolian");
+        }
+
+        [Fact]
+        public async Task ConsumeTimedOutEvent_WithUnknownMatchId()
+        {
+            //Arrange
+            var message = Fixture.Create<TimedOut>();
+
+            var hubConnection = HubConnectionHelper.GetHubConnection(HttpMessageHandler, token: _tolianToken, matchId: message.MatchId.ToString());
+
+            SetConnectionHandlers(hubConnection);
+
+            //Act
+            await hubConnection.StartAsync();
+
+            await WaitForResponse(2000);
+
+            //Assert
+            Assert.NotNull(ClientErrorMessage);
+            Assert.Matches("wasn't found", ClientErrorMessage);
+        }
+
+        [Fact]
+        public async Task ConsumeTimedOutEvent_WithInvalidMatchStatus()
+        {
+            //Arrange
+            var message = Fixture.Build<TimedOut>()
+                .With(x => x.MatchId, Guid.Parse("34730E34-4D6E-463D-A9BA-8EC26BEBB63F"))
+                .Create();
+
+            var hubConnection = HubConnectionHelper.GetHubConnection(HttpMessageHandler, token: _tolianToken, matchId: message.MatchId.ToString());
+
+            SetConnectionHandlers(hubConnection);
+
+            //Act
+            await hubConnection.StartAsync();
+
+            Exception? exception = await Record.ExceptionAsync(async () => await Harness.Bus.Publish(message));
+
+            await WaitForResponse(2000);
+
+            //Assert
+            Assert.Null(exception);
+            Assert.Null((await Harness.Published.SelectAsync<MatchFinished>().ToListAsync()).FirstOrDefault(v => v.Context.Message.MatchId == message.MatchId));
+        }
+
+        [Fact]
+        public async Task ConsumeTimedOutEvent_WithValidInputAsBlack()
+        {
+            //Arrange
+            var message = Fixture.Build<TimedOut>()
+                .With(x => x.MatchId, Guid.Parse("38B56259-55C0-4821-AA4F-D83ED7B58FDF"))
+                .With(x => x.TimedOutSide, 0)
+                .Create();
+
+            var hubConnection = HubConnectionHelper.GetHubConnection(HttpMessageHandler, token: _kolianToken, matchId: message.MatchId.ToString());
+
+            SetConnectionHandlers(hubConnection);
+
+            //Act
+            await hubConnection.StartAsync();
+
+            Exception? exception = await Record.ExceptionAsync(async () => await Harness.Bus.Publish(message));
+
+            await WaitForResponse(2000);
+
+            //Assert
+            Assert.Null(exception);
+            Assert.NotNull(_matchFinishedDto);
+            Assert.Equal(message.MatchId, _matchFinishedDto.MatchId);
+            Assert.Equal("Kolian", _matchFinishedDto.Winner);
+            Assert.Equal("OnTime", _matchFinishedDto.WinBy);
+            Assert.Null(_matchFinishedDto.DrawBy);
+            Assert.NotNull((await Harness.Published.SelectAsync<MatchFinished>().ToListAsync()).FirstOrDefault(v => v.Context.Message.MatchId == message.MatchId));
+        }
+
+        [Fact]
+        public async Task ConsumeTimedOutEvent_WithValidInputAsWhite()
+        {
+            //Arrange
+            var message = Fixture.Build<TimedOut>()
+                .With(x => x.MatchId, Guid.Parse("3979B95F-BA5D-4EF7-8405-C9D23BD9609E"))
+                .With(x => x.TimedOutSide, 1)
+                .Create();
+
+            var hubConnection = HubConnectionHelper.GetHubConnection(HttpMessageHandler, token: _kolianToken, matchId: message.MatchId.ToString());
+
+            SetConnectionHandlers(hubConnection);
+
+            //Act
+            await hubConnection.StartAsync();
+
+            Exception? exception = await Record.ExceptionAsync(async () => await Harness.Bus.Publish(message));
+
+            await WaitForResponse(2000);
+
+            //Assert
+            Assert.Null(exception);
+            Assert.NotNull(_matchFinishedDto);
+            Assert.Equal(message.MatchId, _matchFinishedDto.MatchId);
+            Assert.Equal("Tolian", _matchFinishedDto.Winner);
+            Assert.Equal("OnTime", _matchFinishedDto.WinBy);
+            Assert.Null(_matchFinishedDto.DrawBy);
+            Assert.NotNull((await Harness.Published.SelectAsync<MatchFinished>().ToListAsync()).FirstOrDefault(v => v.Context.Message.MatchId == message.MatchId));
+        }
+
+        public override Task DisposeAsync()
+        {
+            _matchFinishedDto = null;
+
+            return base.DisposeAsync();
+        }
+
+        protected override void SetConnectionHandlers(HubConnection hubConnection)
+        {
+            base.SetConnectionHandlers(hubConnection);
+
+            hubConnection.On<MatchFinishedDto>("MatchFinished", (dto) =>
+            {
+                _matchFinishedDto = dto;
+
+                ResponseReceived = true;
+            });
+        }
+    }
+}
---
+++ tests/MatchService.UnitTests/TimedOutConsumerTests.cs
@@ -0,0 +1,121 @@
+﻿using AutoFixture;
+using EventsLib;
+using MassTransit;
+using MatchService.Consumers;
+using MatchService.DTOs;
+using MatchService.Exceptions;
+using MatchService.Features;
+using MatchService.Models;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using Match = MatchService.Models.Match;
+
+namespace MatchService.UnitTests
+{
+    public class TimedOutConsumerTests : HandlerTestsBase
+    {
+        private readonly Mock<IHubContext<MatchHub>> _hubContextMock;
+        private readonly Mock<ConsumeContext<TimedOut>> _consumeContextMock;
+        private readonly Mock<IHubClients> _clientsMock;
+        private readonly Mock<ISingleClientProxy> _clientProxyMock;
+
+        public TimedOutConsumerTests()
+        {
+            _hubContextMock = new Mock<IHubContext<MatchHub>>();
+            _consumeContextMock = new Mock<ConsumeContext<TimedOut>>();
+            _clientsMock = new Mock<IHubClients>();
+            _clientProxyMock = new Mock<ISingleClientProxy>();
+        }
+
+        [Fact]
+        public async Task Consume_WithNullMatch_ThrowsMatchNotFoundException()
+        {
+            //Arrange
+            var sut = new TimedOutConsumer(MatchRepositoryMock.Object, PublishEndpoint.Object, MapperMock.Object, _hubContextMock.Object);
+            var message = Fixture.Create<TimedOut>();
+
+            MatchRepositoryMock.Setup(x => x.GetMatchById(message.MatchId)).ReturnsAsync((Match?)null);
+            _consumeContextMock.Setup(x => x.Message).Returns(message);
+
+            //Act
+
+            //Assert
+            await Assert.ThrowsAsync<MatchNotFoundException>(() => sut.Consume(_consumeContextMock.Object));
+        }
+
+        [Fact]
+        public async Task Consume_WithNotInProgressMatchStatus_ThrowsNoException()
+        {
+            //Arrange
+            var sut = new TimedOutConsumer(MatchRepositoryMock.Object, PublishEndpoint.Object, MapperMock.Object, _hubContextMock.Object);
+            var message = Fixture.Create<TimedOut>();
+            var matchStatus = Fixture.Create<Generator<MatchStatus>>().First(s => s != MatchStatus.InProgress);
+            var match = Fixture.Build<Match>()
+                .With(x => x.Status, matchStatus)
+                .Create();
+
+            MatchRepositoryMock.Setup(x => x.GetMatchById(message.MatchId)).ReturnsAsync(match);
+            _consumeContextMock.Setup(x => x.Message).Returns(message);
+
+            //Act
+            Exception? exception = await Record.ExceptionAsync(() => sut.Consume(_consumeContextMock.Object));
+
+            //Assert
+            Assert.Null(exception);
+
+            MatchRepositoryMock.Verify(x => x.RemoveMatch(It.IsAny<Match>()), Times.Never);
+            MatchRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Never);
+            MapperMock.Verify(x => x.Map<MatchFinished>(It.IsAny<Match>()), Times.Never);
+            MapperMock.Verify(x => x.Map<MatchFinishedDto>(It.IsAny<Match>()), Times.Never);
+            PublishEndpoint.Verify(x => x.Publish(It.IsAny<MatchFinished>(), CancellationToken.None), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public async Task Consume_WithValidInputAsWhiteSide_ReturnsCorrectData(int timeoutSide)
+        {
+            //Arrange
+            var sut = new TimedOutConsumer(MatchRepositoryMock.Object, PublishEndpoint.Object, MapperMock.Object, _hubContextMock.Object);
+            var message = Fixture.Build<TimedOut>()
+                .With(x => x.TimedOutSide, timeoutSide)
+                .Create();
+            var match = Fixture.Build<Match>()
+                .With(x => x.Status, MatchStatus.InProgress)
+                .Create();
+
+            string? winner = null;
+
+            if (timeoutSide == 0)
+                winner = match.WhiteSidePlayer == match.Creator ? match.Acceptor : match.Creator;
+            else if (timeoutSide == 1)
+                winner = match.WhiteSidePlayer == match.Creator ? match.Creator : match.Acceptor;
+
+            MatchRepositoryMock.Setup(x => x.GetMatchById(message.MatchId)).ReturnsAsync(match);
+            _consumeContextMock.Setup(x => x.Message).Returns(message);
+            _hubContextMock.Setup(x => x.Clients).Returns(_clientsMock.Object);
+            _clientsMock.Setup(clients => clients.Group(message.MatchId.ToString())).Returns(_clientProxyMock.Object);
+
+            //Act
+            Exception? exception = await Record.ExceptionAsync(() => sut.Consume(_consumeContextMock.Object));
+
+            //Assert
+            Assert.Null(exception);
+            Assert.NotNull(match.EndedAtUtc);
+            Assert.InRange(match.EndedAtUtc.Value, DateTime.UtcNow - TimeSpan.FromSeconds(1), DateTime.UtcNow);
+            Assert.NotNull(match.Winner);
+            Assert.NotNull(match.WinBy);
+            Assert.Null(match.DrawBy);
+            Assert.Equal(MatchStatus.Finished, match.Status);
+            Assert.Equal(WinDescriptor.OnTime, match.WinBy);
+            Assert.Equal(winner, match.Winner);
+
+            MatchRepositoryMock.Verify(x => x.RemoveMatch(It.IsAny<Match>()), Times.Once);
+            MatchRepositoryMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+            MapperMock.Verify(x => x.Map<MatchFinished>(It.IsAny<Match>()), Times.Once);
+            MapperMock.Verify(x => x.Map<MatchFinishedDto>(It.IsAny<Match>()), Times.Once);
+            PublishEndpoint.Verify(x => x.Publish(It.IsAny<MatchFinished>(), CancellationToken.None), Times.Once);
+            _clientProxyMock.Verify(c => c.SendCoreAsync("MatchFinished", new object[] { It.IsAny<MatchFinishedDto>() }, CancellationToken.None), Times.Once);
+        }
+    }
+}
