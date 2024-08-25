24bf70765580e0436c928b7be625dd090a043f4a
akaWild <nemyx84@gmail.com>
2024-08-25 12:12:09 +0300

    Fixed small typos in draw accept handler

--- api/MatchService/Features/AcceptDraw/AcceptDrawHandler.cs
+++ api/MatchService/Features/AcceptDraw/AcceptDrawHandler.cs
@@ -12,14 +12,14 @@ namespace MatchService.Features.AcceptDraw
     public record AcceptDrawCommand(Guid MatchId, string? User)
         : ICommand<MatchFinishedDto>;
 
-    public class AcceptDrawHandler : ICommandHandler<AcceptDrawCommand, MatchFinishedDto>
+    public class RejectDrawHandler : ICommandHandler<AcceptDrawCommand, MatchFinishedDto>
     {
         private readonly IMatchRepository _matchRepo;
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly IMapper _mapper;
         private readonly ILocalExpirationService _expService;
 
-        public AcceptDrawHandler(IMatchRepository matchRepo, IPublishEndpoint publishEndpoint, IMapper mapper, ILocalExpirationService expService)
+        public RejectDrawHandler(IMatchRepository matchRepo, IPublishEndpoint publishEndpoint, IMapper mapper, ILocalExpirationService expService)
         {
             _matchRepo = matchRepo;
             _publishEndpoint = publishEndpoint;
@@ -37,10 +37,10 @@ namespace MatchService.Features.AcceptDraw
                 throw new MatchNotFoundException($"Match with id {request.MatchId} wasn't found");
 
             if (request.User != match.Creator && request.User != match.Acceptor)
-                throw new DrawAcceptException("Draw can be requested only by match participant");
+                throw new DrawAcceptException("Draw can be accepted only by match participant");
 
             if (match.Status != MatchStatus.InProgress)
-                throw new DrawAcceptException("Draw can be requested only on active match");
+                throw new DrawAcceptException("Draw can be accepted only on active match");
 
             if (match.DrawRequestedSide == null)
                 throw new DrawAcceptException("Can't accept draw because there wasn't previous request");
